884017bd856c964bee6be6b7ef12d53a2402d29f
Maensh <is4syria@gmail.com>
2019-10-17 17:48:47 +0300

    edit java file

--- Fraction/src/fraction/Fraction.java
+++ Fraction/src/fraction/Fraction.java
@@ -17,6 +17,37 @@ public class Fraction {
      * @param args the command line arguments
      */
     
+    int numerator;
+    int denominator;
+ 
+    /**
+    * Constructor
+    * 
+    * @param numr
+    * @param denr
+    */
+    public Fraction(int numr, int denr) {
+	numerator = numr;
+	denominator = denr;
+	//reduce();
+    }
+ 
+    public int getNumerator() {
+	return numerator;
+    }
+ 
+    public void setNumerator(int numerator) {
+	this.numerator = numerator;
+    }
+ 
+    public int getDenominator() {
+	return denominator;
+    }
+ 
+    public void setDenominator(int denominator) {
+	this.denominator = denominator;
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
